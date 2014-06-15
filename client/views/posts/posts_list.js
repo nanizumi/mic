3f02b04bbb4cff8911baf9aa2cf2bdff3d7d3bd4
nanizumi <nanizumi@gmail.com>
2014-06-15 15:03:01 +0100

    comm3-1

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+    {
+        title: 'introducing çeretqsãa',
+        author: 'mememme',
+        url: 'http://fome.com'
+    },
+    {
+        title: 'cof cof',
+        author: 'piruliii',
+        url: 'http://right.there'
+    },
+    {
+        title: 'the 1234456',
+        author: 'poisix',
+        url: 'http://poisiscx.com'
+    }
+];
+Template.postsList.helpers({
+        posts: postsData
+    });
