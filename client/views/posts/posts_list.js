20b0f9a7db0badf067747110aa55f1c17276e100
nanizumi <nanizumi@gmail.com>
2014-06-16 12:29:05 +0100

    comm4-4

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postsData = [
-    {
-        title: 'introducing çeretqsãa',
-        author: 'mememme',
-        url: 'http://fome.com'
-    },
-    {
-        title: 'cof cof',
-        author: 'piruliii',
-        url: 'http://right.there'
-    },
-    {
-        title: 'the 1234456',
-        author: 'poisix',
-        url: 'http://poisiscx.com'
-    }
-];
 Template.postsList.helpers({
-        posts: postsData
+        posts: function() {
+          return Posts.find();
+        }
     });
