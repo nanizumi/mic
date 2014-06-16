ec181a47f44e6a1deb54ab7a6b68eb33e213dfe8
nanizumi <nanizumi@gmail.com>
2014-06-16 15:51:12 +0100

    comm5-2 pulled form original

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,11 +4,13 @@ if (Posts.find().count() === 0) {
     author: 'Sacha Greif',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
+  
   Posts.insert({
     title: 'Meteor',
     author: 'Tom Coleman',
     url: 'http://meteor.com'
   });
+  
   Posts.insert({
     title: 'The Meteor Book',
     author: 'Tom Coleman',
