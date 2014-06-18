8c209b3176e84e964f36e891c2394ea97ef06427
nanizumi <nanizumi@gmail.com>
2014-06-18 22:45:54 +0100

    comm 7-6

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -2,15 +2,19 @@
  * Created by vagrant on 6/17/14.
  */
 
-Template.postSubmit.events({
-  'submit form': function(e) {
-    e.preventDefault();
+Template.postSubmit.events ({
+  'submit form': function (e) {
+    e.preventDefault ();
     var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val(),
-      message: $(e.target).find('[name=message]').val()
+      url: $ (e.target).find ('[name=url]').val (),
+      title: $ (e.target).find ('[name=title]').val (),
+      message: $ (e.target).find ('[name=message]').val ()
     };
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+
+    Meteor.call ('post', post, function (error, id) {
+      if (error)
+        return alert (error.reason);
+      Router.go ('postPage', {_id: id});
+    });
   }
 });
