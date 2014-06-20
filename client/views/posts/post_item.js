23163fc16e554b6df14fb8f150e0aaf59a21c5a6
nanizumi <nanizumi@gmail.com>
2014-06-20 12:02:23 +0100

    comm 10-3

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,13 +1,16 @@
-Template.postItem.helpers({
+Template.postItem.helpers ({
   ownPost: function () {
-    return this.userId == Meteor.userId();
+    if (!Meteor.userId ())
+      return false;
+
+    return this.userId == Meteor.userId ();
   },
-  domain: function() {
-    var a = document.createElement('a');
+  domain: function () {
+    var a = document.createElement ('a');
     a.href = this.url;
     return a.hostname;
   },
   commentCount: function () {
-    return Comments.find({postId: this._id}).count();
+    return Comments.find ({postId: this._id}).count ();
   }
 });
--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,27 @@
-Comments = new Meteor.Collection('comments');
+Comments = new Meteor.Collection ('comments');
+
+Meteor.methods ({
+  comment: function (commentAttributes) {
+    var user = Meteor.user ();
+    var post = Posts.findOne (commentAttributes.postId);
+
+    if (!user)
+      throw new Meteor.Error (401, "You need to login to make comments");
+
+    if (!commentAttributes.body){
+      //console.log(commentAttributes);
+      throw new Meteor.Error (422, "Please write some content");
+    }
+
+    if (!post)
+      throw new Meteor.Error (422, "You must comment on a post");
+
+    comment = _.extend (_.pick (commentAttributes, 'postId', 'body'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date ().getTime ()
+    });
+
+    return Comments.insert(comment);
+  }
+});
