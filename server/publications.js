06284ad6ae248142745fd19fc2aef5616a8d788f
nanizumi <nanizumi@gmail.com>
2014-06-21 23:56:42 +0100

    comm 12-6

--- server/publications.js
+++ server/publications.js
@@ -1,11 +1,15 @@
-Meteor.publish('posts', function(options) {
-  return Posts.find({}, options);
+Meteor.publish ('posts', function (options) {
+  return Posts.find ({}, options);
 });
 
-Meteor.publish('comments', function(postId) {
-  return Comments.find({postId: postId});
+Meteor.publish ('singlePost', function (id) {
+  return id && Posts.find (id);
 });
 
-Meteor.publish('notifications', function() {
-  return Notifications.find({userId: this.userId});
+Meteor.publish ('comments', function (postId) {
+  return Comments.find ({postId: postId});
+});
+
+Meteor.publish ('notifications', function () {
+  return Notifications.find ({userId: this.userId});
 });
